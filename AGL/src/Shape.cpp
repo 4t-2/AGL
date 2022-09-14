0fa876ce773d7c2877a13ee870cbfe4af3dda34c
4t-2 <zbfddx@protonmail.com>
2022-09-14 23:06:03 +0100

    Shape translation and scale via transform matrix

--- AGL/src/Shape.cpp
+++ AGL/src/Shape.cpp
@@ -2,6 +2,9 @@
 #include <cstdio>
 #include <cstdlib>
 #include <glm/ext/matrix_float4x4.hpp>
+#include <glm/ext/matrix_transform.hpp>
+#include <glm/ext/vector_float3.hpp>
+#include <glm/ext/vector_float4.hpp>
 
 agl::Shape::~Shape()
 {
@@ -49,17 +52,43 @@ void agl::Shape::setColor(agl::Color color)
 	return;
 }
 
+void transform(glm::mat4 &mat, agl::Vec3f size, agl::Vec3f position)
+{
+	mat[3][3] = 1.0f;
+
+	mat[0][0] = size.x;
+	mat[1][1] = size.y;
+	mat[2][2] = size.z;
+
+	mat[3][0] = position.x;
+	mat[3][1] = position.y;
+	mat[3][2] = position.z;
+
+	return;
+}
+
 void agl::Shape::setShapeData()
 {
 	int vertices = (shapeData.getVertexDataSize() / sizeof(float)) / 3;
+
+	glm::mat4 transformMatrix = glm::mat4();
+	transform(transformMatrix, size, position);
 
 	float *newVertexData = (float *)malloc(shapeData.getVertexDataSize());
 
 	for (int i = 0; i < vertices; i++)
 	{
-		newVertexData[(i * 3) + 0] = (vertexData[(i * 3) + 0] * size.x) + position.x;
-		newVertexData[(i * 3) + 1] = (vertexData[(i * 3) + 1] * size.y) + position.y;
-		newVertexData[(i * 3) + 2] = (vertexData[(i * 3) + 2] * size.z) + position.z;
+		glm::vec4 oldPos;
+		oldPos.x = vertexData[(i * 3) + 0];
+		oldPos.y = vertexData[(i * 3) + 1];
+		oldPos.z = vertexData[(i * 3) + 2];
+		oldPos.w = 1.0f;
+
+		glm::vec4 newPos = transformMatrix * oldPos;
+
+		newVertexData[(i * 3) + 0] = newPos.x;
+		newVertexData[(i * 3) + 1] = newPos.y;
+		newVertexData[(i * 3) + 2] = newPos.z;
 	}
 
 	shapeData.setVertexData(newVertexData, shapeData.getVertexDataSize());
