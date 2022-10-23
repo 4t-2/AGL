0d9691682afd17589befb837e1d4df80edddd9ce
4t-2 <zbfddx@protonmail.com>
2022-10-23 21:07:28 +0100

    Added Mat4f << operator overloading

--- AGL/include/Mat4f.hpp
+++ AGL/include/Mat4f.hpp
@@ -2,7 +2,9 @@
 
 #include "Vec3f.hpp"
 #include "Vec4f.hpp"
-#include <cstddef>
+
+#include <cstdio>
+#include <iostream>
 
 namespace agl
 {
@@ -18,9 +20,27 @@ namespace agl
 
 			Mat4f operator*(Mat4f matrix);
 			Vec4f operator*(Vec4f vector);
-			
+
+			friend std::ostream &operator<<(std::ostream &os, const Mat4f &mat)
+			{
+				char output[161];
+
+				sprintf(output,
+						"%+08f %+08f %+08f %+08f\n"											//
+						"%+08f %+08f %+08f %+08f\n"											//
+						"%+08f %+08f %+08f %+08f\n"											//
+						"%+08f %+08f %+08f %+08f\n",										//
+						mat.data[0][0], mat.data[1][0], mat.data[2][0], mat.data[3][0], //
+						mat.data[0][1], mat.data[1][1], mat.data[2][1], mat.data[3][1], //
+						mat.data[0][2], mat.data[1][2], mat.data[2][2], mat.data[3][2], //
+						mat.data[0][3], mat.data[1][3], mat.data[2][3], mat.data[3][3]	//
+				);
+
+				return os << output;
+			}
+
 			void clear();
-			
+
 			void identity();
 			void translate(Vec3f translation);
 			void scale(Vec3f scale);
