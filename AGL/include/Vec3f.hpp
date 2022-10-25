f19c6667888fd43e32988a038889ef653c8c1441
4t-2 <zbfddx@protonmail.com>
2022-10-25 18:31:55 +0100

    Added << overloading to Vec3f

--- AGL/include/Vec3f.hpp
+++ AGL/include/Vec3f.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <ostream>
+
 namespace agl
 {
 	class Vec3f
@@ -9,6 +11,14 @@ namespace agl
 			float y = 0;
 			float z = 0;
 
-		Vec3f operator-(Vec3f vector);
+			Vec3f				 operator-(Vec3f vector);
+			friend std::ostream &operator<<(std::ostream &os, const Vec3f &vec)
+			{
+				char output[31];
+
+				sprintf(output, "%+08f %+08f %+08f\n", vec.x, vec.y, vec.z);
+
+				return os << output;
+			}
 	};
 } // namespace agl
